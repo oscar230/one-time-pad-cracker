35bf36768e4d8b994ca5307bd24b999b61eace18
Oscar Andersson <oscar.andersson.200@gmail.com>
2020-04-14 23:45:57 +0200

    dragger done

--- main.go
+++ main.go
@@ -4,12 +4,13 @@ import (
 	"bufio"
 	"encoding/hex"
 	"os"
+	"strings"
 
 	"github.com/fatih/color"
 )
 
 func main() {
-	wordlist := []string{" ", ".", "the", "be", "to", "of", "and", "a", "in", "that", "have", "I", "it", "for", "not", "on", "with", "he", "as", "you", "do", "at", "this", "but", "his", "by", "from", "they", "we", "say", "her", "she", "or", "an", "will", "my", "one", "all", "would", "there", "thier", "what", "hello", "world"}
+	wordlist := []string{"the", "and", "that", "have", "for", "not", "with", "you", "this", "but", "his", "from", "they", "say", "her", "she", "will", "one", "all", "would", "there", "thier", "what", "hello", "world", "program"}
 
 	f, err := os.Open("cipher")
 	check(err)
@@ -22,27 +23,38 @@ func main() {
 
 	color.New(color.FgYellow).Printf("This is the \"One-Time Pad cracker\" by Oscar Andersson at KAU.\n")
 	color.New(color.FgYellow).Printf("Cipherkeys:\t%d\nWordlist:\t\t%d\n", len(cipherkeys), len(wordlist))
-	color.New(color.FgGreen).Add(color.Bold).Printf("Keys:\t\t\t%+v\n", crack(cipherkeys, wordlist))
+	crack(cipherkeys, wordlist)
 }
 
-func crack(cipherkeys [][]byte, wordlist []string) (keys []string) {
-	drag(cipherkeys[0], cipherkeys[1], "the")
-	return keys
+func crack(cipherkeys [][]byte, wordlist []string) {
+	for _, word := range wordlist {
+		drag(cipherkeys[0], cipherkeys[1], word, wordlist)
+	}
 }
 
-func drag(ct1, ct2 []byte, word string) {
+func drag(ct1, ct2 []byte, word string, wordlist []string) string {
 	wordx := []byte(word)
 	ctx := xorbytes(ct1, ct2)
-	color.New(color.FgWhite).Printf("Dragging:\n\tword: %s = %x\n\tct1: %x\n\tct2: %x\n\tctx: %x\n\n", word, wordx, ct1, ct2, ctx)
+	// color.New(color.FgWhite).Printf("Dragging:\n\tword: %s = %x\n\tct1: %x\n\tct2: %x\n\tctx: %x\n", word, wordx, ct1, ct2, ctx)
 	for i := 0; i < len(ctx)-len(wordx)+1; i++ {
 		result := xorbytes(ctx[i:i+len(wordx)], wordx)
-		color.New(color.FgWhite).Printf("\t\tDrag %x", ctx[0:i])
-		color.New(color.FgGreen).Printf("%x", ctx[i:i+len(wordx)])
-		if i+len(wordx) < len(ctx) {
-			color.New(color.FgWhite).Printf("%x", ctx[i+len(wordx):len(ctx)])
+		// color.New(color.FgWhite).Printf("\t\tDrag %x", ctx[0:i])
+		// color.New(color.FgGreen).Printf("%x", ctx[i:i+len(wordx)])
+		// if i+len(wordx) < len(ctx) {
+		// 	color.New(color.FgWhite).Printf("%x", ctx[i+len(wordx):len(ctx)])
+		// }
+		// color.New(color.FgWhite).Printf(" at %d => %x %s\n", i, result, result)
+		for _, w := range wordlist {
+			if strings.Contains(strings.ToLower(string(result)), w) {
+				color.New(color.FgHiWhite).Add(color.Bold).Printf("\t\tFound")
+				color.New(color.FgHiBlue).Add(color.Bold).Printf(" %s ", result)
+				color.New(color.FgHiWhite).Add(color.Bold).Printf("matched in wordlist at")
+				color.New(color.FgHiGreen).Add(color.Bold).Printf(" %s\n", w)
+				return w
+			}
 		}
-		color.New(color.FgWhite).Printf(" at %d result: %x\n", i, result)
 	}
+	return ""
 }
 
 func xorbytes(a, b []byte) (c []byte) {
