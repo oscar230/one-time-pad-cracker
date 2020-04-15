8251b30fc7dda914e85a692d283969925d128841
Oscar Andersson <oscar.andersson.200@gmail.com>
2020-04-15 19:06:25 +0200

    remove duplicate problem

--- main.go
+++ main.go
@@ -10,41 +10,34 @@ import (
 )
 
 func main() {
-	color.New(color.FgYellow).Printf("This is the \"One-Time Pad cracker\" by Oscar Andersson at KAU.\n")
+	color.New(color.FgYellow).Add(color.Bold).Add(color.Underline).Printf("This is the \"One-Time Pad cracker\" by Oscar Andersson at KAU.\n")
 
 	cipherkeys := loadcipher()
 	wordlist := loadwordlist()
 
-	color.New(color.FgYellow).Printf("Cipherkeys: %d\nWordlist: %d\nCombinations: %d\n", len(cipherkeys), len(wordlist), len(cipherkeys)*len(cipherkeys)*len(wordlist))
+	color.New(color.FgYellow).Printf("Cipherkeys: %d\nWordlist: %d\nCombinations: %d\n", len(cipherkeys), len(wordlist), len(cipherkeys)*len(cipherkeys)*len(wordlist)-len(cipherkeys))
 
 	crack(cipherkeys, wordlist)
-}
-
-func scan(cipherkeys [][]byte, dragged [][]string, wordlist []string) {
-	for i, _ := range cipherkeys {
-		color.New(color.FgHiCyan).Printf("Cipher %d with %d dragged words.\n", i, len(dragged[i]))
-		// for _, draggedword := range dragged[i] {
-		// 	color.New(color.FgHiCyan).Printf("\t%s\n", draggedword)
-		// }
-	}
 }
 
 func crack(cipherkeys [][]byte, wordlist []string) {
 	var dragged = make([][]string, len(cipherkeys))
 
 	// Drag phase
-	color.New(color.FgGreen).Add(color.Bold).Printf("Start multithreaded dragging.")
+	color.New(color.FgGreen).Add(color.Bold).Add(color.Underline).Printf("Start multithreaded dragging.\n")
 	timestart := time.Now()
 	for i, c1 := range cipherkeys {
-		for _, c2 := range cipherkeys {
-			for _, word := range wordlist {
-				var dragchannel chan []string = make(chan []string)
-				go drag(c1, c2, word, dragchannel)
-				var dw = <-dragchannel
-				dragged[i] = append(dragged[i], dw...)
+		for j, c2 := range cipherkeys {
+			if i != j {
+				for _, word := range wordlist {
+					var dragchannel chan []string = make(chan []string)
+					go drag(c1, c2, word, dragchannel)
+					var dw = <-dragchannel
+					dragged[i] = append(dragged[i], dw...)
+				}
 			}
 		}
-		color.New(color.FgGreen).Printf("Cipher %d done.\n", i)
+		color.New(color.FgGreen).Printf("-> Cipher %d done.\n", i)
 	}
 	color.New(color.FgGreen).Add(color.Bold).Printf("All ciphers total time: %ds.\n", time.Now().Unix()-timestart.Unix())
 
@@ -52,25 +45,27 @@ func crack(cipherkeys [][]byte, wordlist []string) {
 	scan(cipherkeys, dragged, wordlist)
 }
 
+func scan(cipherkeys [][]byte, dragged [][]string, wordlist []string) {
+	color.New(color.FgHiCyan).Add(color.Bold).Add(color.Underline).Printf("Scanning of dragged words.\n")
+	for i := range cipherkeys {
+		color.New(color.FgCyan).Printf("-> Cipher %d with %d dragged words.\n", i, len(dragged[i]))
+
+		// for _, dw := range dragged[i] {
+		// 	color.New(color.FgCyan).Printf("\t%s\n", dw)
+		// }
+	}
+}
+
 func drag(ct1, ct2 []byte, word string, dragchannel chan []string) (dragged []string) {
 	wordx := []byte(word)
 	ctx := xorbytes(ct1, ct2)
 	passes := len(ctx) - len(wordx) + 1
-	if passes < 0 {
-		passes = 0
-	}
-	// color.New(color.FgWhite).Printf("Cipher: ")
-	// color.New(color.FgHiGreen).Printf("%x", ct1)
-	// color.New(color.FgWhite).Printf(" & ")
-	// color.New(color.FgHiGreen).Printf("%x", ct2)
-	// color.New(color.FgWhite).Printf("\twill do ")
-	// color.New(color.FgHiGreen).Printf("%d", passes)
-	// color.New(color.FgWhite).Printf("\tpasses with word: ")
-	// color.New(color.FgHiGreen).Printf("%s\n", word)
+
 	for i := 0; i < passes; i++ {
 		result := xorbytes(ctx[i:i+len(wordx)], wordx)
 		dragged = append(dragged, string(result))
 	}
+
 	dragchannel <- dragged
 	return
 }
